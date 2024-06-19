a5193a9e797e21b783d14050ab29b0bd4f5c1222
Maruf Murshed <marufmurshed9255@gmail.com>
2024-06-20 00:24:57 +0600

    endpoints added successfully

---
+++ allEndpoints.js
@@ -0,0 +1,93 @@
+//crate a single notice
+// post
+// /create-notice
+// {
+//     "title": "Eid holiday notice als",
+//     "description": "school is off from next thursday"
+// }
+
+
+
+
+//get all notice
+// get
+// /all-notice
+
+
+
+
+
+//crate a single research
+// post
+// /create-research
+// {
+//     "title": "Ensemble Ml Designing for the very first time",
+//     "description": "this research is all about system Designing and extra things"
+// }
+
+
+
+
+
+//get all research
+// get
+// /all-research
+
+
+
+//create a single user
+// post
+// /registration-user
+// {
+//     "email": "marufmurshed9255@gmail.com",
+//     "password": "123xx",
+//     "role": "teacher"
+// }
+
+
+
+
+
+// logged in of a user
+// post
+// /login-user
+// {
+//     "email": "junechakma@gmail.com",
+//     "password": "123xx"
+// }
+
+
+
+
+
+// get all users
+// get
+// /all-users
+
+
+
+
+
+//delete a single user
+//delete
+// /users/:id
+// users/66718ccc6effacb16dd3bc74
+
+
+
+
+
+
+//delete a single notice
+//delete
+// /notice/:id
+// notice/66718ccc6effacb16dd3bc74
+
+
+
+
+
+//delete a single research
+//delete
+// /research/:id
+// research/66718ccc6effacb16dd3bc74
